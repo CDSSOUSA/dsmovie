541252699cbf1f5b7a2be4ce86fb6579233328ed
CDSSOUSA <cleberdossantossousa@gmail.com>
2022-05-11 11:47:31 -0300

    Find movies

--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -18,19 +18,15 @@ public class ScorePK implements Serializable {
     public ScorePK(){
 
     }
-
     public Movie getMovie() {
         return movie;
     }
-
     public void setMovie(Movie movie) {
         this.movie = movie;
     }
-
     public User getUser() {
         return user;
     }
-
     public void setUser(User user) {
         this.user = user;
     }
